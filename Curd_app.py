5b47353ca7dc105e12b2134f276f9621d60f57e4
vishalmanes109 <36571896+vishalmanes109@users.noreply.github.com>
2020-11-18 22:14:10 +0530

    validation implemented

--- Curd_app.py
+++ Curd_app.py
@@ -2,6 +2,8 @@ import mysql.connector
 import os
 from os.path import join, dirname
 from dotenv import load_dotenv
+import datetime
+import re
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -21,6 +23,23 @@ mydb = mysql.connector.connect(
 # creating databse cursor
 cursor = mydb.cursor()
 
+
+def validmobile(mobileno):
+
+    isvalid = re.compile("[7-9][0-9]{9}")
+    data=isvalid.match(mobileno)
+    return data
+
+
+def validdob(s):
+  date_format = '%Y-%m-%d'
+
+  try:
+    date_obj = datetime.datetime.strptime(s, date_format)
+    return True
+  except ValueError:
+    return False
+
 # add employee method
 def addemployee(ename,eadd,edob,emobile):
   print("Adding Employee in to databse...")
@@ -30,9 +49,11 @@ def addemployee(ename,eadd,edob,emobile):
   cursor.execute(sql, val)
 
   mydb.commit()
-
-  print(cursor.rowcount, "record inserted.")
-  print("Employee Added")
+  if(cursor.rowcount==0):
+    print("ERROR: EMPLOYEE IS NOT ADDED")
+  else:  
+    print(cursor.rowcount, "record inserted.")
+    print("Employee Added")
 
 def deleteemployee(ename,eid):
   print("Deleting Employee...")
@@ -41,9 +62,11 @@ def deleteemployee(ename,eid):
   cursor.execute(sql, eid)
 
   mydb.commit()
-
-  print(cursor.rowcount, "record(s) deleted")
-  print("Employee Deleted")
+  if (cursor.rowcount==0):
+    print("ERROR: EMPLOYEE IS NOT DELETED  ")
+  else:  
+    print(cursor.rowcount, "record(s) deleted")
+    print("Employee Deleted")
 
 
 def updateeployee(ename,eid):
@@ -62,12 +85,17 @@ def updateeployee(ename,eid):
     cursor.execute(sql, val)
 
     mydb.commit()
-
-    print(cursor.rowcount, "record(s) affected")
-    print("employee Updated")
+    if (cursor.rowcount == 0):
+      print("ERROR: EMPLOYEE IS NOT UPDATED  ")
+    else:
+      print(cursor.rowcount, "record(s) affected")
+      print("employee Updated")
 
   elif(sel==2):
-    edob = input("Enter The new DOB (YY-MM-DD): ").strip()
+    edob = input("Enter The new DOB (YYYY-MM-DD): ").strip()
+    if(validdob(edob) == False):
+        print("please enter DOB in proper formate(YYYY-MM-DD)")
+        updateeployee(ename,eid)
 
     print("updating Employee")
 
@@ -78,11 +106,17 @@ def updateeployee(ename,eid):
 
     mydb.commit()
 
-    print(cursor.rowcount, "record(s) affected")
-    print("employee Updated") 
+    if (cursor.rowcount == 0):
+      print("ERROR: EMPLOYEE IS NOT UPDATED  ")
+    else:
+      print(cursor.rowcount, "record(s) affected")
+      print("employee Updated")
     
   elif(sel==3):
     emobile = input("Enter The new Mobile No : ").strip()
+    if(validmobile(emobile) == None):
+        print("Please Enter valid Mobile Number ")
+        updateeployee(ename,eid)
 
     print("updating Employee")
 
@@ -93,8 +127,11 @@ def updateeployee(ename,eid):
 
     mydb.commit()
 
-    print(cursor.rowcount, "record(s) affected")
-    print("employee Updated")
+    if (cursor.rowcount == 0):
+      print("ERROR: EMPLOYEE IS NOT UPDATED  ")
+    else:
+      print(cursor.rowcount, "record(s) affected")
+      print("employee Updated")
 
 
 
@@ -125,6 +162,9 @@ def searchemployee():
 
   elif(sel == 2):
     ename = input("Enter Name of Employee which has to be search: ").strip().title()
+    if (ename.replace(" ", "").isalpha() == False):
+        print("please Enter valid Name (A-Z a-z no numbers or special characters)")
+        searchemployee()
     sql = ("SELECT * FROM employee where ename=%s")
     ename=(ename,)
     cursor.execute(sql, ename)
@@ -144,23 +184,30 @@ def searchemployee():
         print("**END**")
 
   elif(sel == 3):
-    emobile = input("Enter Mobile no  of Employee which has to be Search: ")
-    sql = ("SELECT * FROM employee where emobile=%s")
-    emobile=(emobile,)
-    cursor.execute(sql, emobile)
+    today_date = date.today()
+    #print(today_date)
+    if(validdob(today_date)==False):
+      print("enter Valid Date (YYYY-MM-DD)")
+      searchemployee()
+    age = float(input("Enter age of Employee which has to be Search: ").strip())
+    sql = ("select * , DATEDIFF(%s, edob) / 365.25 as age from employee")
+    nowdate=(today_date,)
+    cursor.execute(sql, nowdate)
     res = cursor.fetchall()
 
     if(len(res) == 0):
       print("No record Found")
     else:
       for emp in res:
-        print("**Employee Details**")
-        print("id: ", emp[0])
-        print("name :", emp[1])
-        print("address: ", emp[2])
-        print("DOB: ", emp[3])
-        print("Mobile No: ", emp[4])
-        print("**END**")
+        if(float(emp[5])>=age):
+          print("**Employee Details**")
+          print("id: ", emp[0])
+          print("name :", emp[1])
+          print("address: ", emp[2])
+          print("DOB: ", emp[3])
+          print("Mobile No: ", emp[4])
+          print("Age: ",emp[5])
+          print("**END**")
 
   else:
     print("Enter Valid choice for searching")
@@ -192,11 +239,26 @@ def menu():
     choice = int(input("Enter Your Choice: ").strip())
 
     if(choice == 1):
-      ename = input("Enter Name: ").strip().title()
+      ename = input("Enter Name:  ").strip().title()
+      
+      
       eadd = input("Enter Address: ").strip().title()
-      edob = input("Enter Date Of Birth (YY-MM-DD): ").strip()
+      edob = input("Enter Date Of Birth (YYYY-MM-DD): ").strip()
       emobile = input("ENter Mobile No: ").strip()
+      if (ename.replace(" ", "").isalpha() == False):
+        print ("please Enter valid Name (A-Z a-z no numbers or special characters)")
+        menu()
+
+      if(validdob(edob)==False):
+        print("please enter DOB in proper formate(YYYY-MM-DD)")  
+        menu()
+
+      if(validmobile(emobile) ==None):
+        print("Please Enter valid Mobile Number ")  
+        menu()
+
       addemployee(ename, eadd, edob, emobile)
+
     elif(choice == 2):
       ename = input(
           "Enter Name of employee which has to be updated: ").strip().title()
